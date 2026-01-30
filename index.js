e4b251293f07bff705c3f94314a5b03430aaa3b5
Maicon dos Reis <110362232+SrReiss@users.noreply.github.com>
2026-01-30 10:57:36 -0300

    index.js
    Criação do validador de idade

---
+++ index.js
@@ -0,0 +1,7 @@
+const idadeMaior = 18;
+const idadeIdoso = 60;
+const idade = 60;
+const verificadorMaior = idade >= idadeMaior;
+const verificadorIdoso = idade >= idadeIdoso;
+console.log("Você é maior de idade: " + verificadorMaior);
+console.log("Você é idoso: " + verificadorIdoso);
